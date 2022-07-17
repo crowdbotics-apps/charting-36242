08253c96539e04136b02f95f095c42842e2e08aa
Crowdbotics <team@crowdbotics.com>
2022-07-17 16:03:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/charting_36242/MainActivity.java
@@ -0,0 +1,15 @@
+package com.charting_36242;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "charting_36242";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "charting_36242",
+  url: "https://charting_36242.botics.co",
+  api: "https://charting_36242.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
